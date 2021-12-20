30f816b372b382264377f7cc1be5549605dbbb62
Jordan Larios <jolarios96@gmail.com>
2021-12-19 23:10:07 -0800

    initial route file

---
+++ server/db/conn.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
